8985bd48bc2d29f5b6d75f4433e6b5295fce7035
Denys <denys.zaluzhnyi@gmail.com>
2019-10-02 07:37:16 +0300

    Fix App tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Redirect, BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {act} from 'react-dom/test-utils';
+import {Route} from 'react-router-dom';
 import {shallow} from 'enzyme';
-import ShallowRenderer from 'react-test-renderer/shallow';
 import App from './../app';
 import HomePage from '../pages/home';
+import LoginPage from '../pages/login';
+import LogoutPage from '../pages/logout';
 
 
-let container;
 let pathMap = {};
 
-beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-    const component = shallow(<App/>);
-    console.log(component);
+describe('App tests', () => {
+    beforeAll(() => {
+        const component = shallow(<App/>);
 
-    pathMap = component.find(Route).reduce((pathMap, route) => {
-        const routeProps = route.props;
-        pathMap[routeProps.path] = routeProps.component;
-        return pathMap;
-    }, {});
+        pathMap = component.find(Route).reduce((pathMap, route) => {
+            const routeProps = route.props();
+            pathMap[routeProps.path] = routeProps.component;
+            return pathMap;
+        }, {});
+    });
 
+    it('Should show home component', () => {
+        expect(pathMap['/']).toBe(HomePage);
+    });
+
+    it('Should show login component', () => {
+        expect(pathMap['/login']).toBe(LoginPage);
+    });
+
+    it('Should show logout component', () => {
+        expect(pathMap['/logout']).toBe(LogoutPage);
+    });
 });
 
 
-afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-});
-
-it('Should show home component', () => {
-    expect(pathMap['/']).toBe(HomePage);
-});
 
 
