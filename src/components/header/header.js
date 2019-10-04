80e580036657e5a53f0d49d7b98d70a7d4e759aa
DarkDream99 <denys.zaluzhnyi@gmail.com>
2019-10-04 11:58:55 +0300

    Add types for header and page-title components

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {Jumbotron} from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 import './header.css';
 
 
-const Header = (props) => {
+const Header = ({title}) => {
     return (
         <Jumbotron fluid className='header'>
-            {props.title}
+            {title}
         </Jumbotron>
     );
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header;
--- src/components/page-title/page-title.js
+++ src/components/page-title/page-title.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './page-title.css';
 
 
 const PageTitle = ({title}) => {
-    console.log(title);
     return (
         <h1 className="page-title">{title}</h1>
     );
+}
+
+PageTitle.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default PageTitle;
